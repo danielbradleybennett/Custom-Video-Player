ace65682fdaeaeee9b563191bfd88e57c1acadad
DanielBradleyBennett <danielbradleybennett@gmail.com>
2021-01-16 22:20:14 -0600

    need to fix progress bar

--- scripts.js
+++ scripts.js
@@ -31,6 +31,15 @@ function handleRangeUpdate() {
   //console.log(this.value);
 }
 
+function handleProgress() {
+  const percent = (video.currtentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
 
 
 //hook up the event listeners
@@ -38,8 +47,16 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
 
