10d6bb30c5d90056675744229c472629b36aadad
DanielBradleyBennett <danielbradleybennett@gmail.com>
2021-01-17 19:14:43 -0600

    progress bar works

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const player = document.querySelector('.player');
-const video = player.querySelector('.viewer')
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
@@ -7,34 +7,33 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 function togglePlay() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+
   //const method = video.paused ? 'play' : 'pause';
   // video[method]();
 }
 
 function updateButton() {
   const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
   toggle.textContent = icon;
 }
 
+
 function skip() {
-  console.log(this.dataset.skip);
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function handleRangeUpdate() {
   video[this.name] = this.value;
-  //console.log(this.value);
 }
 
 function handleProgress() {
-  const percent = (video.currtentTime / video.duration) * 100;
+  const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
 }
+
 
 function scrub(e) {
   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
@@ -46,10 +45,10 @@ function scrub(e) {
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
-toggle.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', handleProgress);
-skipButtons.forEach(button => button.addEventListener('click', skip));
 
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
